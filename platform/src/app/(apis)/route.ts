af83dbb32fc41058f76bdd04380b28f072e66572
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-05-17 04:50:14 -0500

    feat: added platform stub

---
+++ platform/src/app/(apis)/route.ts
@@ -0,0 +1,12 @@
+import config from '@/payload.config'
+import { getPayload } from 'payload'
+
+export const GET = async () => {
+  const payload = await getPayload({ config })
+
+  const data = await payload.find({
+    collection: 'users',
+  })
+
+  return Response.json(data)
+}
