82dc582a5250e3a410e2148624c7a1e8e4f2f5ba
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-05-17 09:27:58 -0500

    fix: missing deps

--- platform/src/collections/Functions.ts
+++ platform/src/collections/Functions.ts
@@ -10,25 +10,46 @@ export const Functions: CollectionConfig = {
   },
   versions: true,
   fields: [
+    { name: 'name', type: 'text', required: true, unique: true, admin: { position: 'sidebar' } },
     {
-      type: 'row',
-      fields: [
-        { name: 'name', type: 'text', required: true, unique: true },
-        {
-          name: 'output',
-          type: 'select',
-          defaultValue: 'Object',
-          options: ['Object', 'ObjectArray', 'Text', 'TextArray', 'Code'],
-        },
-        {
-          name: 'model',
-          // type: 'text',
-          type: 'select',
-          options: await getModels(),
-          defaultValue: ({ req }) => req.payload.findGlobal({ slug: 'settings' }).then((settings) => settings.defaultModel || 'google/gemini-2.5-pro-preview'),
-        },
-      ],
+      name: 'output',
+      type: 'select',
+      defaultValue: 'Object',
+      options: ['Object', 'ObjectArray', 'Text', 'TextArray', 'Code'],
+      admin: { position: 'sidebar' },
     },
+    { 
+      name: 'object', 
+      type: 'relationship', 
+      relationTo: 'nouns', 
+      admin: { position: 'sidebar', condition: (data) => data.output === 'Object' || data.output === 'ObjectArray' } },
+    {
+      name: 'model',
+      // type: 'text',
+      type: 'select',
+      options: await getModels(),
+      // defaultValue: ({ req }) => req.payload.findGlobal({ slug: 'settings' }).then((settings) => settings.defaultModel || 'google/gemini-2.5-pro-preview'),
+      admin: { position: 'sidebar' },
+    },
+    // {
+    //   type: 'row',
+    //   fields: [
+    //     { name: 'name', type: 'text', required: true, unique: true },
+    //     {
+    //       name: 'output',
+    //       type: 'select',
+    //       defaultValue: 'Object',
+    //       options: ['Object', 'ObjectArray', 'Text', 'TextArray', 'Code'],
+    //     },
+    //     {
+    //       name: 'model',
+    //       // type: 'text',
+    //       type: 'select',
+    //       options: await getModels(),
+    //       // defaultValue: ({ req }) => req.payload.findGlobal({ slug: 'settings' }).then((settings) => settings.defaultModel || 'google/gemini-2.5-pro-preview'),
+    //     },
+    //   ],
+    // },
     { name: 'system', type: 'code', admin: { language: 'mdx', editorOptions } },
     { name: 'prompt', type: 'code', admin: { language: 'mdx', editorOptions } },
     {
@@ -47,6 +68,6 @@ export const Functions: CollectionConfig = {
       admin: { language: 'yaml', editorOptions },
     },
     { name: 'data', type: 'json', admin: { hidden: true } },
-    { name: 'executions', type: 'join', collection: 'events', on: 'execution', hasMany: true },
+    // { name: 'executions', type: 'join', collection: 'events', on: 'execution', hasMany: true },
   ],
 }
