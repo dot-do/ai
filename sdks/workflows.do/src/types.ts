f420e1b8b70a7064ac0bccac31c677378be3f2ac
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-05-18 06:23:54 -0500

    fix: types and workflow implementaiton

--- sdks/workflows.do/src/types.ts
+++ sdks/workflows.do/src/types.ts
@@ -3,3 +3,11 @@ export type AIWorkflowDefinition = Record<string, any>
 export type OnEvent = {}
 
 export type Workflow = {}
+
+export type AIInstance = {
+  
+}
+
+export type AIFunction = {
+  
+}
